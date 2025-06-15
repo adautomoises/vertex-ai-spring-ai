1adbd2a0e0f146b74d56c3a9163932d383dda6ef
Adauto Moisés <adautomaleandro@gmail.com>
2025-06-15 14:50:08 -0300

    Inicializando repositório

---
+++ src/main/java/br/com/adautomoises/vertex_ai_spring_ai/VertexAiSpringAiApplication.java
@@ -0,0 +1,13 @@
+package br.com.adautomoises.vertex_ai_spring_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VertexAiSpringAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VertexAiSpringAiApplication.class, args);
+	}
+
+}
